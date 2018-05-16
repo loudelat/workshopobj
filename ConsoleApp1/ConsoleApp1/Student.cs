4cd6c038f70ce6c830e1ab531ee93041da52459e
Unknown <loudelat@amazon.com>
2018-05-16 15:44:37 -0700

    Created Student class and instantiated first object

---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        string courseFocus;
+        string firstName;
+        string lastName;
+
+        // constructor
+        public Student()
+        {
+            Console.WriteLine("Student object created!");
+        }
+    }
+}
